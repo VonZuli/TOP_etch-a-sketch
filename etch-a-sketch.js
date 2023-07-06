2b0ac2348c9064da291d325f51aea834c79a12e0
Mike Camenzuli <36173249+VonZuli@users.noreply.github.com>
2023-07-06 17:59:39 -0400

    created initial logic and styling for etch-a-sketch app

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const body = document.querySelector("body");
+const container = document.createElement("div");
+container.style.display = "flex";
+container.id = "container";
+body.appendChild(container);
+
+function buildCanvas() {
+  for (let i = 0; i < 16 * 16; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.className = "gridItem";
+    gridItem.id = `gridItem-${i}`;
+    gridItem.textContent = i;
+    container.appendChild(gridItem);
+
+    gridItem.addEventListener("mouseenter", draw);
+  }
+}
+
+buildCanvas();
+
+function draw() {
+  const gridItem = document.querySelector(".gridItem");
+
+  this.classList.add("colored");
+}
