b6b73f6a7f6e58fdbb9cbcc608e62d91779b4fb8
Mike Camenzuli <36173249+VonZuli@users.noreply.github.com>
2023-07-07 17:01:20 -0400

    added function for changing size of grid area and adjusted some property names

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,27 +1,60 @@
 "use strict";
-
 const body = document.querySelector("body");
 const container = document.createElement("div");
-container.style.display = "flex";
-container.id = "container";
-body.appendChild(container);
+const resetCanvasBtn = document.createElement("button");
+// const gridItem = document.querySelector(".canvasItem");
 
 function buildCanvas() {
-  for (let i = 0; i < 16 * 16; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.className = "gridItem";
-    gridItem.id = `gridItem-${i}`;
-    gridItem.textContent = i;
-    container.appendChild(gridItem);
+  container.style.display = "flex";
+  container.id = "container";
+  resetCanvasBtn.id = "resetCanvasBtn";
+  resetCanvasBtn.textContent = "Select Canvas Size";
 
-    gridItem.addEventListener("mouseenter", draw);
+  resetCanvasBtn.addEventListener("click", resizeCanvas);
+
+  body.appendChild(resetCanvasBtn);
+  body.appendChild(container);
+
+  for (let i = 0; i < 16 * 16; i++) {
+    const canvasItem = document.createElement("div");
+    canvasItem.className = "canvasItem";
+    canvasItem.id = `canvasItem-${i}`;
+    canvasItem.textContent = i;
+    container.appendChild(canvasItem);
+
+    canvasItem.addEventListener("mouseenter", draw);
   }
 }
 
 buildCanvas();
 
 function draw() {
-  const gridItem = document.querySelector(".gridItem");
-
   this.classList.add("colored");
+}
+
+function resizeCanvas() {
+  let userPrompt = prompt(
+    "Please enter the size of canvas you would like (all entries are squared): "
+  );
+
+  let userEntry = parseInt(userPrompt);
+
+  if (isNaN(userEntry) || userEntry > 64 || userEntry <= 0) {
+    alert("Please enter a number greater than 0 and less than or equal to 20.");
+    resizeCanvas();
+  } else {
+    const container = document.getElementById("container");
+    while (container.firstChild) {
+      container.removeChild(container.lastChild);
+    }
+    for (let i = 0; i < userEntry * userEntry; i++) {
+      const canvasItem = document.createElement("div");
+      canvasItem.className = "canvasItem";
+      canvasItem.id = `canvasItem-${i}`;
+      canvasItem.textContent = i;
+      container.appendChild(canvasItem);
+
+      canvasItem.addEventListener("mouseenter", draw);
+    }
+  }
 }
